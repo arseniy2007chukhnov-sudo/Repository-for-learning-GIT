be823918d068d7751882249aad43e046fdcc032f
root <root@LAPTOP-3I9FKNT7.localdomain>
2026-02-24 16:34:34 +0800

    added data info checker

---
+++ bash-practice/linux_as_execenv/pinger.py
@@ -0,0 +1,8 @@
+import requests
+from datetime import datetime, timedelta
+
+url_template = "https://simurg.space/gen_file?data=obs&date={date}"
+url = url_template.format(date="2026-02-20")
+
+response = requests.get(url = url, stream=True)
+print(f"From url {url} got {response}")
