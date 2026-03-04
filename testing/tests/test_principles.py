4f61c18e3eef09d706ca081a2212056e2d14fa75
root <root@LAPTOP-3I9FKNT7.localdomain>
2026-03-04 14:43:21 +0800

    Show need-to-avoid test

--- testing/tests/test_principles.py
+++ testing/tests/test_principles.py
@@ -2,8 +2,7 @@ import sys
 # TODO make with 'pip install -e .
 sys.path.append("../src")
 
-from math_demo import add
-from math_demo import add_with_bug
+from math_demo import (add, add_with_bug)
 
 def test_addition_basic():
     assert add(2,2) == 4, "Function didn't returned 4" 
@@ -13,12 +12,22 @@ def test_bug_addition_notsufficient():
     assert add_with_bug(2,2) == 4, "Function didn't returned 4" 
     print("Test BUG ADDITION PASSED")
 
+
 def test_bug_addition_enough():
     assert add_with_bug(2, 2) == 4, "Function didn't returned"
     assert add_with_bug(0, 0) == 0
     assert add_with_bug(5, 6) == 11
     print("Test BUG ADDITION PASSED")
 
+def test_addition_duplicated_logic():
+    # VERY BAD TEST -
+    assert add(6, 3) == 6 + 3
+    # VERY GOOD TEST +
+    assert add(6, 3) == 9
+    print ("Test DUPLICATED LOGIC PASSED")
+
 if __name__ == "__main__":
     test_addition_basic()
     test_bug_addition_notsufficient()
+    test_addition_duplicated_logic()
+    test_bug_addition_enough()
