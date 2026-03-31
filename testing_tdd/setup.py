582f50a31746ca1fad465827c0c4a0724db7e18b
root <root@LAPTOP-3I9FKNT7.localdomain>
2026-03-31 23:38:26 +0800

    Added automatization with makefile and added .gitignore

--- testing_tdd/setup.py
+++ testing_tdd/setup.py
@@ -1,9 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = "ndfl",
+    name = "ndfl-aosu",
     version = "0.0.0",
-
+    long_description = "Tax calculator",
+    long_description_content_type = "text/markdown",
     package_dir = {"": "src"},
-    packages = find_packages(where="src")
+    packages = find_packages(where="src"),
+    author ='Arseniy Chukhnov',
+    author_email='arseniy2007chukhnov@gmail.com'
 )
